a04c6e5c7ee25bb9a2c916abe7929016ecd40a7b
samlach2222 <lachaudsamuel@protonmail.com>
2023-04-14 15:06:14 +0200

    Add Template display for wires

---
+++ BrodUI/Models/Wire.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Media;
+
+namespace BrodUI.Models
+{
+    public class Wire
+    {
+        private Brush _color; // RGB color who are used in the image
+        private int _number; // TYPE number (DMC number, VàC number, etc.)
+        private string _type; // DMC, VàC, etc.
+        private string _name; // TYPE name (DMC name, VàC name, etc.)
+        private int _quantity; // quantity of the wire
+
+        public Brush Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public Wire(Brush color, int number, string type, string name, int quantity)
+        {
+            Color = color;
+            Number = number;
+            Type = type;
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
--- BrodUI/ViewModels/ExportViewModel.cs
+++ BrodUI/ViewModels/ExportViewModel.cs
@@ -1,7 +1,8 @@
 ﻿using BrodUI.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
-using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Media;
 using Wpf.Ui.Common.Interfaces;
 
 namespace BrodUI.ViewModels
@@ -9,6 +10,7 @@ namespace BrodUI.ViewModels
     public partial class ExportViewModel : ObservableObject, INavigationAware
     {
         private Uri? _loadedImage = null;
+        private List<Wire> _wireArray = new List<Wire>();
 
         private ImageManagement Im { get; set; }
 
@@ -24,8 +26,24 @@ namespace BrodUI.ViewModels
             }
         }
 
+        public List<Wire> WireArray
+        {
+            get { return _wireArray; }
+            set
+            {
+                _wireArray = value;
+                OnPropertyChanged();
+            }
+        }
+
         public void OnNavigatedTo()
         {
+            // HERE IS TEMPLATE VALUES
+            WireArray.Add(new Wire(new SolidColorBrush(Color.FromArgb(255, (byte)255, (byte)255, (byte)255)), 0, "DMC", "White", 0));
+            WireArray.Add(new Wire(new SolidColorBrush(Color.FromArgb(255, (byte)0, (byte)255, (byte)255)), 0, "DMC", "White", 0));
+            WireArray.Add(new Wire(new SolidColorBrush(Color.FromArgb(255, (byte)255, (byte)0, (byte)255)), 0, "DMC", "White", 0));
+            WireArray.Add(new Wire(new SolidColorBrush(Color.FromArgb(255, (byte)255, (byte)255, (byte)0)), 0, "DMC", "White", 0));
+            // END OF TEMPLATE VALUES
             LogManagement.WriteToLog("[" + DateTime.Now.ToString() + "] " + Assets.Languages.Resource.Terminal_ExportPage);
             if (Im == null) // if not already initialized
             {
