34e1bda2c65eb8c93df129cfd8fad8eb3d4cf1eb
samlach2222 <lachaudsamuel@protonmail.com>
2023-05-05 15:14:48 +0200

    Add Brush2DtoColorDict class test template

---
+++ BrodUITests/HelpersTests/Brush2DtoColorDictTests.cs
@@ -0,0 +1,13 @@
+﻿using BrodUI.Helpers;
+using Xunit;
+
+namespace BrodUITests.HelpersTests
+{
+    public class Brush2DtoColorDictTests
+    {
+        [Fact]
+        public void FunctionName()
+        {
+        }
+    }
+}
