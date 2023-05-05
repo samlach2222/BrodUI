f314aa5963628bb9f9ffa4ba1b99811ebdecf73c
samlach2222 <lachaudsamuel@protonmail.com>
2023-05-05 17:14:05 +0200

    [Tests] Wire OK

---
+++ BrodUITests/ModelsTests/WireTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+using BrodUI.Models;
+using Xunit;
+
+namespace BrodUITests.ModelsTests
+{
+    public class WireTests
+    {
+        [Fact]
+        public void WireTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal(new SolidColorBrush(Color.FromRgb(255,0,0)).ToString(), wire.Color.ToString());
+            Assert.Equal(255, wire.Number);
+            Assert.Equal("DMC", wire.Type);
+            Assert.Equal("RED", wire.Name);
+            Assert.Equal(20, wire.Quantity);
+        }
+
+        [Fact]
+        public void ColorTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal(new SolidColorBrush(Color.FromRgb(255,0,0)).ToString(), wire.Color.ToString());
+            wire.Color = new SolidColorBrush(Color.FromRgb(0,255,0));
+            Assert.Equal(new SolidColorBrush(Color.FromRgb(0,255,0)).ToString(), wire.Color.ToString());
+        }
+
+        [Fact]
+        public void NumberTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal(255, wire.Number);
+            wire.Number = 0;
+            Assert.Equal(0, wire.Number);
+        }
+
+        [Fact]
+        public void TypeTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal("DMC", wire.Type);
+            wire.Type = "Anchor";
+            Assert.Equal("Anchor", wire.Type);
+        }
+
+        [Fact]
+        public void NameTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal("RED", wire.Name);
+            wire.Name = "GREEN";
+            Assert.Equal("GREEN", wire.Name);
+        }
+
+        [Fact]
+        public void QuantityTest()
+        {
+            Wire wire = new(new SolidColorBrush(Color.FromRgb(255,0,0)), 255, "DMC", "RED", 20);
+            Assert.Equal(20, wire.Quantity);
+            wire.Quantity = 0;
+            Assert.Equal(0, wire.Quantity);
+        }
+    }
+}
+
