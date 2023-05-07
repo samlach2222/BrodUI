55625c6836520bc8b3d994dfe10cedfdc984ab9f
ZeinaAl-Halabi <92623259+ZeinaAl-Halabi@users.noreply.github.com>
2023-05-07 19:03:29 +0200

    Add DMCtoStringTests.cs

---
+++ BrodUITests/HelpersTests/DMCtoStringTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using BrodUI.Helpers;
+using System;
+
+namespace BrodUITests.HelpersTests
+{ 
+    public class DMCtoStringTests
+	{
+		public void GetNameDmcTests()
+		{
+			DMCtoString d = new DMCtoString();
+			var actual = d.GetNameDmc(517);
+			string expected = "Dark Wedgwood";
+			Assert.Equal(actual, expected);
+		}
+	}
+}
